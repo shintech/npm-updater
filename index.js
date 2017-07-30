125cdd37ea0ebda29082469d9507d2a3f9ea1b47
Michael Prather <mprather1@gmail.com>
2017-07-30 07:21:08 +0000

    0.0.2

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 const path = require('path')
 const chalk = require('chalk')
-const currentDir = process.env['DIR']
+const currentDir = process.argv[2]
 const pkg = path.join(currentDir, 'package.json')
 const _package = require(pkg)
 var fs = require('fs')
 
 const p = _package
 
-console.log(`writing version ${chalk.green(process.argv[2])} to package.json...`)
-p.version = process.argv[2]
+console.log(`writing version ${chalk.green(process.argv[3])} to package.json...`)
+p.version = process.argv[3]
 
 function getValues (obj) {
-  let string = ''
-  const values = Object.keys(obj)
-  values.map(function (value) {
-    string += `    "${value}": "${obj[value]}"`
-    if (values.indexOf(value) !== values.length - 1) {
-      string += ',\n'
-    }
-  })
-  return string
+  if (obj !== undefined) {
+    let string = ''
+    const values = Object.keys(obj)
+    values.map(function (value) {
+      string += `    "${value}": "${obj[value]}"`
+      if (values.indexOf(value) !== values.length - 1) {
+        string += ',\n'
+      }
+    })
+    return string
+  } else {
+    throw new Error('object in package.json is undefined...')
+  }
 }
 
 var retval = `{
