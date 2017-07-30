387d030b4a2be3a3e4119f3d62460e76bd11ea9a
Michael Prather <mprather1@gmail.com>
2017-07-30 19:07:46 +0000

    0.0.3

--- index.js
+++ index.js
@@ -1,64 +1,79 @@
+var fs = require('fs')
 const path = require('path')
 const chalk = require('chalk')
+const yaml = require('js-yaml')
+const logger = require('winston-color')
+
 const currentDir = process.argv[2]
 const pkg = path.join(currentDir, 'package.json')
-const _package = require(pkg)
-var fs = require('fs')
+const config = path.join(currentDir, 'version.yml')
+const version = process.argv[3]
+var regex = /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(-(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(\.(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*)?(\+[0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*)?$/
 
-const p = _package
+validate(run)
 
-console.log(`writing version ${chalk.green(process.argv[3])} to package.json...`)
-p.version = process.argv[3]
+function run () {
+  checkForPkg()
+  checkForYaml()
+}
 
-function getValues (obj) {
-  if (obj !== undefined) {
-    let string = ''
-    const values = Object.keys(obj)
-    values.map(function (value) {
-      string += `    "${value}": "${obj[value]}"`
-      if (values.indexOf(value) !== values.length - 1) {
-        string += ',\n'
-      }
+function validate (cb) {
+  if (version === undefined) {
+    handleError('version argument is undefined...')
+    handleError('please try again...\n')
+    return false
+  }
+
+  if (!regex.test(version)) {
+    handleError('invalid argument for version number...')
+    handleError('please try again...\n')
+    return false
+  }
+
+  cb()
+}
+
+function checkForPkg () {
+  fs.stat(pkg, (err, res) => {
+    if (err && err.path === pkg) {
+      logger.warn('package.json not found...\naborting...\n')
+      return
+    } else if (err) {
+      throw new Error(err)
+    }
+
+    const p = require(pkg)
+    detailedInfo('package.json')
+    p.version = process.argv[3]
+    fs.writeFile(pkg, JSON.stringify(p, null, 2))
+  })
+}
+
+function checkForYaml () {
+  try {
+    const v = yaml.safeLoad(fs.readFileSync(config, 'utf8'))
+    v.version = version
+
+    detailedInfo('version.yml')
+    const y = yaml.safeDump(v, {
+      'sortkeys': true
     })
-    return string
-  } else {
-    throw new Error('object in package.json is undefined...')
+
+    fs.writeFile(config, y)
+  } catch (err) {
+    if (err && err.path === config) {
+      logger.warn('version.yml not found...\naborting...\n')
+    } else {
+      throw new Error(err)
+    }
   }
 }
 
-var retval = `{
-  "name": "${p.name}",
-  "version": "${p.version}",
-  "main": "${p.main}",
-  "license": "${p.license}",
-  "repository":"${p.repository}",
-  "author": "${p.author}",
-  "scripts": {
-${getValues(p['scripts'])}
-  },
-  "devDependencies": {
-${getValues(p['devDependencies'])}
-  },
-  "dependencies": {
-${getValues(p['dependencies'])}
-  },
-  "standard": {
-    "globals": [
-      "it",
-      "describe",
-      "beforeEach",
-      "afterEach",
-      "before",
-      "after",
-      "Backbone",
-      "_",
-      "$"
-    ]
-  },
-  "engines": {
-    "node": ">=6"
-  }
+function detailedInfo (file) {
+  logger.info(`${chalk.yellow(file)} was found...`)
+  logger.info(`writing version ${chalk.green(process.argv[3])} to ${chalk.yellow(file)}...`)
 }
- `
 
-fs.writeFile(pkg, retval)
+function handleError (err) {
+  logger.error(err)
+}
