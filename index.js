0dd009de550cbc6fbe16807cef9ccfe702a8efd4
Michael Prather <mprather1@gmail.com>
2018-11-22 04:25:18 +0000

    refactor with promises

--- index.js
+++ index.js
@@ -1,79 +1,55 @@
 var fs = require('fs')
 const path = require('path')
 const chalk = require('chalk')
-const yaml = require('js-yaml')
 const logger = require('winston-color')
+const compare = require("compare-versions")
 
 const currentDir = process.argv[2]
 const pkg = path.join(currentDir, 'package.json')
-const config = path.join(currentDir, 'version.yml')
 const version = process.argv[3]
 var regex = /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(-(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(\.(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*)?(\+[0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*)?$/
 
-validate(run)
-
-function run () {
-  checkForPkg()
-  checkForYaml()
+if (version === undefined) {
+  return logger.error('version argument is "undefined"...' )
 }
 
-function validate (cb) {
-  if (version === undefined) {
-    handleError('version argument is undefined...')
-    handleError('please try again...\n')
-    return false
-  }
+if (!regex.test(version)) {
+  return logger.error('invalid argument for version number...')
+}
 
-  if (!regex.test(version)) {
-    handleError('invalid argument for version number...')
-    handleError('please try again...\n')
-    return false
-  }
+main()
 
-  cb()
+// ----------------------------------------------------------------------------
+
+async function main () {
+  try {
+    await checkForPkg()
+
+    logger.info(`${chalk.yellow('package.json')} was found...`)
+    logger.info(`writing version ${chalk.green(process.argv[3])} to ${chalk.yellow('package.json')}...`)    
+  } catch (err) {
+    logger.error(err.message)
+  }
 }
 
 function checkForPkg () {
-  fs.stat(pkg, (err, res) => {
-    if (err && err.path === pkg) {
-      logger.warn('package.json not found...\naborting...\n')
-      return
-    } else if (err) {
-      throw new Error(err)
-    }
+  return new Promise (function (resolve, reject) {
+    fs.stat(pkg, (err, res) => {
+      if (err && err.path === pkg) {
+        reject({ message: 'pcakage.json not found...'})
+      } else if (err) {
+        reject(err.message)
+      }
 
-    const p = require(pkg)
-    detailedInfo('package.json')
-    p.version = process.argv[3]
-    fs.writeFile(pkg, JSON.stringify(p, null, 2))
-  })
-}
+      const p = require(pkg)
+      console.log(compare(version, p.version))
+      if (compare(version, p.version) <= 0) { return reject({ message: `package.json - argument is lower than previous version -> ${p.version}` }) }
 
-function checkForYaml () {
-  try {
-    const v = yaml.safeLoad(fs.readFileSync(config, 'utf8'))
-    v.version = version
+      p.version = process.argv[3]
 
-    detailedInfo('version.yml')
-    const y = yaml.safeDump(v, {
-      'sortkeys': true
+      fs.writeFile(pkg, JSON.stringify(p, null, 2), () => {
+        resolve(pkg)
+      })
     })
-
-    fs.writeFile(config, y)
-  } catch (err) {
-    if (err && err.path === config) {
-      logger.warn('version.yml not found...\naborting...\n')
-    } else {
-      throw new Error(err)
-    }
-  }
-}
-
-function detailedInfo (file) {
-  logger.info(`${chalk.yellow(file)} was found...`)
-  logger.info(`writing version ${chalk.green(process.argv[3])} to ${chalk.yellow(file)}...`)
-}
-
-function handleError (err) {
-  logger.error(err)
+  })
 }
--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash 
 
-HOME=$HOME /usr/local/bin/node /home/mike/Development/npm-updater/index.js $(pwd) $1
+HOME=$HOME /usr/local/bin/node /home/mprather/Development/npm-updater/index.js $(pwd) $1
